49cdb2c2a3cfcfb5fa1e004a80501a886715720a
Sarah Clements <biting.the.sun@gmail.com>
2016-10-13 18:09:49 -0700

    final version

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+  	pkg: grunt.file.readJSON('package.json'),
+	uglify: {
+		build: {
+			files: {
+		   'dist/js/app.js': 'src/js/app.js',
+		   'dist/js/jquery.js': 'src/js/jquery.js'
+		   
+			}
+		 }
+	},
+	cssmin: {
+	  options: {
+	    shorthandCompacting: false,
+	    roundingPrecision: -1
+	  },
+	  target: {
+	    files: {
+	      'dist/css/style.css': 'src/css/style.css'
+	    }
+	  }
+	},
+	imageoptim: {
+	  build: {
+	    src: ['dist/img', 'dist/img']
+		}
+	  },
+	htmlmin: {                                     
+	    build: {                                     
+	      options: {                                 
+	        removeComments: true,
+	        collapseWhitespace: true
+	      },
+	      files: {                                   
+	        'dist/index.html': 'src/index.html'    
+	      }
+		}
+	 }
+  });
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-imageoptim');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify', 'htmlmin', 'imageoptim', 'cssmin']);
+};
+
+
---
+++ src/js/google-maps-data.js
@@ -0,0 +1,71 @@
+var locations = [{
+        "title": "Dandelion Chocolate",
+        "type": "Cafe's and Bakeries",
+        "location": {
+            "lat": 37.761024, "lng": -122.421779}
+    }, {
+        "title": "Paxton Gate",
+        "type": "Points of Interest",
+        "location": {
+            "lat": 37.759618, "lng": -122.421696}
+    }, {
+        "title": "Dog Eared Books",
+        "type": "Book Stores",
+        "location": {
+            "lat": 37.758404, "lng": -122.4215 }
+    }, {
+        "title": "San Francisco Women's Building",
+        "type": "Points of Interest",
+        "location": {
+            "lat": 37.76156, "lng": -122.422654}
+    }, {
+        "title": "Mission Dolores Park",
+        "type": "Points of Interest",
+        "location": {
+            "lat": 37.759773, "lng": -122.427063}
+    }, {
+        "title": "Four Barrel Coffee",
+        "type": "Cafe's and Bakeries",
+        "location": {
+            "lat": 37.767006, "lng": -122.421885}
+     }, {
+        "title": "The Chapel",
+        "type": "Music and Movies",
+        "location": {
+            "lat": 37.760512, "lng": -122.421247}
+     }, {
+        "title": "Roxie Cinema",
+        "type": "Music and Movies",
+        "location": {
+            "lat": 37.764777, "lng": -122.422403}
+    }, {
+        "title": "Clarion Alley",
+        "type": "Points of Interest",
+        "location": {
+            "lat": 37.762937, "lng": -122.421526}
+    }, {
+        "title": "City Art Gallery",
+        "type": "Art Galleries",
+        "location": {
+            "lat": 37.759477, "lng": -122.421737 }
+    }, {
+        "title": "Misión San Francisco de Asís",
+        "type": "Historical Places",
+        "location": {
+            "lat": 37.764387, "lng": -122.426897 }
+    }, {
+        "title": "The Monk's Kettle",
+        "type": "Restaurants and Bars",
+        "location": {
+            "lat": 37.764728, "lng": -122.422999 }
+    }, {
+        "title": "Ritual Coffee Roasters",
+        "type": "Cafe's and Bakeries",
+        "location": {
+            "lat": 37.756442, "lng": -122.421322 }
+    }, {
+        "title": "Borderlands Books",
+        "type": "Book Stores",
+        "location": {
+            "lat": 37.759045, "lng": -122.421538 }
+    }];
